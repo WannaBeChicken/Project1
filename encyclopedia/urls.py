c7c688e5ca6c8125fb19ca95305ff4a4a0eb0fa5
WannaBeChicken <prathamdavbistupur@gmail.com>
2021-10-19 22:57:09 +0530

    removed search url

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,7 +5,6 @@ from . import views
 urlpatterns = [
     path ("", views.index, name="index"),
     path ("<str:title>", views.title, name="title"),
-    path ("search/",views.search,name="search"),
     path ("create/",views.create,name="create"),
     path ("<str:title>/edit/",views.edit,name="edit")
 ]
